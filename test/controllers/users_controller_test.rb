22efcfeb82fb34b0cda5861b01cc43dc36276390
tatsuya <ryuuma1005@gmail.com>
2019-02-07 19:42:05 +0900

    Finish layout and routes improve

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_signup_path
+    get signup_path
     assert_response :success
   end
 
