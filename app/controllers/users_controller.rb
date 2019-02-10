77ee47232d5889173be776ea006bf4ed4310ca76
tatsuya <ryuuma1005@gmail.com>
2019-02-10 15:30:57 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save 
-      flash[:success] = "Welcome to wweb application"
+      flash[:success] = "Welcome to web application"
+      log_in @user
       redirect_to @user 
     else 
       render 'new'
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,22 @@
+module SessionsHelper
+	#渡されたユーザでログインする
+	#sessionメソッドで作成した一時cookiesは自動的に暗号化される
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+	#現在ログイン中のユーザを返す(いる場合)
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find_by(id: session[:user_id])
+		end
+	end
+	#ユーザがログインしていればtrue、その他ならfalseを返す
+	def logged_in?
+		!current_user.nil?
+	end
+
+	def log_out 
+		session.delete(:user_id)
+		@current_user = nil
+	end
+end
