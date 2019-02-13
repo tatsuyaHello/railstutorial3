23b0956da70cbb6c919abe2092b2f2d09632bc9c
tatsuya <ryuuma1005@gmail.com>
2019-02-13 13:11:24 +0900

    Add account activation

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.5.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 # Use sqlite3 as the database for Active Record 
-gem 'sqlite3', '~> 1.3.6'
+#gem 'sqlite3', '~> 1.3.6'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -45,6 +45,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'sqlite3', '~> 1.3.6'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
@@ -56,6 +57,11 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
+group :production do
+  gem 'pg', '0.20.0'
+end
+
 
 group :test do
   # add new gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
-	before_save { self.email = email.downcase }
+	attr_accessor :remember_token, :activation_token 
+	before_save :downcase_email
+	before_create :create_activation_digest
 	validates :name, 
 		presence: true,
 		length: {maximum: 50}
@@ -32,13 +33,27 @@ class User < ApplicationRecord
 		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	# トークンがダイジェストと一致したらtrueを返す
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	#ユーザのログイン情報を破棄する
 	def forget 
 		update_attribute(:remember_digest, nil)
 	end
+
+	private
+		#メールアドレスを全て小文字にする
+		def downcase_email
+			self.email = email.downcase 
+		end
+
+		#有効かトークンとダイジェストを作成及び代入する
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
 end
